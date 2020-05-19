9b6adab2296e85b41ed4ed48ba2e4d4a2b2e08d4
Nicholas Wylynko <1014375@student.ccgs.wa.edu.au>
2020-05-19 20:35:52 +0800

    fix styling, add auto publish

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 export default function time(timestamp: number, Endings = {
-  S: " Seconds Ago",
-  M: " Minutes Ago",
-  H: " Hours Ago",
-  D: " Days Ago",
-  ELSE: "A really long time ago"
+  seconds: " Seconds Ago",
+  minutes: " Minutes Ago",
+  hours: " Hours Ago",
+  days: " Days Ago",
+  else: "A really long time ago"
 }, nowTimeStamp: number = Date.now()) {
-  let difference: number = nowTimeStamp - timestamp
+  let difference: number = nowTimeStamp - timestamp;
 
   if (difference <= 1000 * 60) {
-    return Math.trunc(difference / 1000) + Endings.S
+    return Math.trunc(difference / 1000) + Endings.seconds;
   } else if (difference <= 1000 * 60 * 60) {
-    return Math.trunc(difference / 1000 / 60) + Endings.M
+    return Math.trunc(difference / 1000 / 60) + Endings.minutes;
   } else if (difference <= 1000 * 60 * 60 * 24) {
-    return Math.trunc(difference / 1000 / 60 / 60) + Endings.H
+    return Math.trunc(difference / 1000 / 60 / 60) + Endings.hours;
   } else if (difference <= 1000 * 60 * 60 * 24 * 365) {
-    return Math.trunc(difference / 1000 / 60 / 60 / 24) + Endings.D
+    return Math.trunc(difference / 1000 / 60 / 60 / 24) + Endings.days;
   } else {
-    return Endings.ELSE
+    return Endings.else;
   }
 }
